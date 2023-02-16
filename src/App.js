be72d10d447d47c6da5399681bf428a55fb7d05e
Tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-02-17 02:48:55 +0300

    şuan deneme yapıyooom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './styles.css'
 
 function App() {
+
+  const [todo, setTodo] = useState('')
+  const {todos, setTodos} = useState([]);
+
+  const addTodo = ()=>{
+    console.log(todo);
+    const item = todo;
+    setTodos(oldItems => [...oldItems, item])
+   
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='todo' >
+      <h1>Todo List App</h1>
+      </div>
+
+      <input
+      value={todo}
+      onChange={e => setTodo(e.target.value)}
+       className='input' type='text' placeholder='Add an item..' ></input>
+
+      <button
+      onClick={()=> addTodo()}
+      >Add</button>
+
+      <div className='todoList' >
+        <ul>
+        {todos.map((item,index) => {
+          <li key={index} >{item} </li>
+        })}
+        </ul>
+
+      </div>
+      
+
+     
     </div>
   );
 }
